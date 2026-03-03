9a714a35ee8ee3913c47f4a5c2108a4ba51fcb3a
Leonardo André Ferreira <145403460+LeonardoAFer@users.noreply.github.com>
2026-03-03 20:51:07 -0300

    First commit

---
+++ DynamicAppointment/src/main/java/com/leonardo/DynamicAppointment/DynamicAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.DynamicAppointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicAppointmentApplication.class, args);
+	}
+
+}
---
+++ DynamicAppointment/src/test/java/com/leonardo/DynamicAppointment/DynamicAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.DynamicAppointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
