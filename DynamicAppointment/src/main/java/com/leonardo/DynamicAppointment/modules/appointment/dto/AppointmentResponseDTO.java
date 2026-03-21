3a3709549ee9777e495811b144692daadc0f8386
Leonardo André Ferreira <145403460+LeonardoAFer@users.noreply.github.com>
2026-03-21 16:50:55 -0300

    Appointment Module
    Appointment CRUD
    Util class updateHelper

--- DynamicAppointment/src/main/java/com/leonardo/DynamicAppointment/modules/appointment/dto/AppointmentResponseDTO.java
+++ DynamicAppointment/src/main/java/com/leonardo/DynamicAppointment/modules/appointment/dto/AppointmentResponseDTO.java
@@ -1,8 +1,35 @@
 package com.leonardo.DynamicAppointment.modules.appointment.dto;
 
+import com.leonardo.DynamicAppointment.modules.appointment.status.AppointmentStatus;
+import com.leonardo.DynamicAppointment.modules.professional.dto.ProfessionalSummaryDTO;
+import com.leonardo.DynamicAppointment.modules.services.dto.BusinessServiceSummaryDTO;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class AppointmentResponseDTO {
+
+    private Long id;
+
+    private String guestName;
+
+    private String guestEmail;
+
+    private String guestPhone;
+
+    private ProfessionalSummaryDTO professional;
+
+    private BusinessServiceSummaryDTO service;
+
+    private LocalDateTime scheduledAt;
+
+    private AppointmentStatus status;
+
+    private String accessToken;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 
 }
