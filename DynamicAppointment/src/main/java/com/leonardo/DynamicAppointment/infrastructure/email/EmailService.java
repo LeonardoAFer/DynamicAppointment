74c00a31d192228405cb8ade45f0dce5dfeea608
LeonardoAFer <145403460+LeonardoAFer@users.noreply.github.com>
2026-03-30 19:07:13 -0300

    EmailService + att readme
    - Initial email service
    - att readme

--- DynamicAppointment/src/main/java/com/leonardo/DynamicAppointment/infrastructure/email/EmailService.java
+++ DynamicAppointment/src/main/java/com/leonardo/DynamicAppointment/infrastructure/email/EmailService.java
@@ -1,8 +1,26 @@
 package com.leonardo.DynamicAppointment.infrastructure.email;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendEmail(String toEmail, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("leoanardo14senai@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        javaMailSender.send(message);
+    }
 
 }
